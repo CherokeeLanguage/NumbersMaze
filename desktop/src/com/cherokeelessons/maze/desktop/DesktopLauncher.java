8b0f568bc5c9e6883c273ce69d3c0efc456ae160
mjoyner <mjoyner@vbservices.net>
2020-01-10 19:00:16 -0500

    initial conversion from old non-gradle libgdx to latest gradle libgdx

---
+++ desktop/src/com/cherokeelessons/maze/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cherokeelessons.maze.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cherokeelessons.maze.NumbersMaze;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NumbersMaze(), config);
+	}
+}
