a979478aca3d335c0884f432e6cf9cfb2c35a828
mjoyner <mjoyner@vbservices.net>
2020-01-12 14:50:49 -0500

    set initial display to 720p windowed.

--- desktop/src/com/cherokeelessons/maze/desktop/DesktopLauncher.java
+++ desktop/src/com/cherokeelessons/maze/desktop/DesktopLauncher.java
@@ -6,7 +6,10 @@ import com.cherokeelessons.maze.NumbersMaze;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		NumbersMaze game = new NumbersMaze();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new NumbersMaze(), config);
+		config.height=720;
+		config.width=1280;
+		new LwjglApplication(game, config);
 	}
 }
